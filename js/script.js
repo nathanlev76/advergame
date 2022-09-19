404b2df23496d21b351c08187cab6f4cdc2f9af3
Dhan-aurel <dboudoh@normandiewebschool.fr>
2022-09-19 10:12:05 +0200

    creat new concepte
    actualisation of game concept

--- js/script.js
+++ js/script.js
@@ -1,17 +1,1 @@
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
-const image = new Image;
-image.onload = drawImageActualSize; 
-
-
-image.src = 'images/fonds.svg';
-
-function drawImageActualSize() {
-
-
-
-  ctx.drawImage(this, 150, 0);
-
-}
